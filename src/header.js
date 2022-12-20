567f2e10302c6137189851b1a52aaae8dff882bd
ahfercen <ahfercen@gmail.com>
2022-12-19 19:53:00 -0500

    css vars for colour scheme

--- src/header.js
+++ src/header.js
@@ -1,15 +1,26 @@
 import logo from "./assets/logo2.webp"
+
 export function generateHeader(){
     const element = document.createElement("div");
     element.className = "header";
     element.appendChild(generateLogo());
+    element.appendChild(generateTabs());
     return element;
 }
+
 function generateLogo(){
     const element = document.createElement("div");
     element.className = 'logo';
     const logoImage = document.createElement("img");
     logoImage.src = logo;
     element.appendChild(logoImage);
+    return element;
+}
+function generateTabs(){
+    const element = document.createElement("div");
+    element.className = 'tabs';
+    let btns = new Array(3);
+
+
     return element;
 }
