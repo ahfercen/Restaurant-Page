88680ce718b7263f36d690ecc098e6ee75b11a97
ahfercen <ahfercen@gmail.com>
2022-12-19 20:15:03 -0500

    tab button onclick boilerplate and styling

--- src/header.js
+++ src/header.js
@@ -1,26 +1,37 @@
 import logo from "./assets/logo2.webp"
+import {switchTab} from "./switch";
 
 export function generateHeader(){
     const element = document.createElement("div");
     element.className = "header";
     element.appendChild(generateLogo());
-    element.appendChild(generateTabs());
+    element.appendChild(generateTabButtons());
     return element;
 }
 
 function generateLogo(){
     const element = document.createElement("div");
     element.className = 'logo';
+
     const logoImage = document.createElement("img");
     logoImage.src = logo;
     element.appendChild(logoImage);
+
     return element;
 }
-function generateTabs(){
+function generateTabButtons(){
+    const btnText = ["Home","Menu","Contact"];
+
     const element = document.createElement("div");
     element.className = 'tabs';
-    let btns = new Array(3);
 
+    for(let i = 0;i<btnText.length;i++){
+        const b = document.createElement("button");
+        b.onclick = switchTab;
+        b.innerHTML = btnText[i];
+        b.className="tabBtn";
+        element.appendChild(b);
+    }
 
     return element;
 }
---
+++ src/switch.js
@@ -0,0 +1,4 @@
+export function switchTab(index){
+    console.log(index);
+    return index;
+}
