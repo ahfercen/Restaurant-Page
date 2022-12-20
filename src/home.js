5e5c25015379f86351f35cdac3d47bfc8e97956a
ahfercen <ahfercen@gmail.com>
2022-12-20 16:41:27 -0500

    Home grid display with images

--- src/home.js
+++ src/home.js
@@ -1,4 +1,7 @@
 import heroImage1 from "./assets/hero1.jpeg";
+import heroImage2 from "./assets/hero6.jpg";
+import heroImage3 from "./assets/hero3.jpg";
+import heroImage4 from "./assets/hero4.jpg";
 
 export function generateHome(){
     const element = document.createElement("div");
@@ -26,10 +29,23 @@ function generateGrid(element){
 function makeHomeData(){
     const a = new Array(4);
     a[0] = [ heroImage1 , "Pasta Pizza Mamma Mia"];
+    a[1] = [ heroImage2 , "Big Energy Food"];
+    a[2] = [ heroImage3 , "Healthy Food For You"];
+    a[3] = [ heroImage4 , "Yummy Yummy Yum Yum!"];
     return a;    
 }
 function generateHeroItem(data){
     console.log(data);
-    return document.createElement("div");
+    const element = document.createElement("div");
+    element.className = "hero-content";
+    const imageDiv = document.createElement("div");
+    imageDiv.className = "hero-img";
+    const image = document.createElement("img");
+    image.src=data[0];
+    imageDiv.appendChild(image);
+
+
+    element.appendChild(imageDiv);
+    return element;
 }
 
