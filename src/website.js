be0e33adf1a726de30041e6fbfb3ec681749c2f7
ahfercen <ahfercen@gmail.com>
2022-12-19 20:42:46 -0500

    Tab flow functions

--- src/website.js
+++ src/website.js
@@ -4,11 +4,22 @@ import {generateHeader} from "./header";
 export function website(){
     const content = document.getElementById("content");
     content.appendChild(generateHeader());
-    content.appendChild(generateTabOne());
+    content.appendChild(tabLoader(1));
 }
 export function changeTab(e){
     console.log(e.srcElement.dataset.value);
     return e;
+}
+
+function tabLoader(index){
+    switch (index) {
+        case 0:
+            return generateTabOne();
+        case 1:
+            return generateTabOne();
+        default:
+            return generateTabOne();
+    }
 }
 
 
